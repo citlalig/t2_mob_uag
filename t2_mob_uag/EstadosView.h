d0e33cee71b06cfb60232f00b4d462a7f115b423
citlalig <citlalig@Citlalis-MacBook-Air.local>
2014-10-10 23:05:16 -0500

    draft 3

---
+++ t2_mob_uag/EstadosView.h
@@ -0,0 +1,13 @@
+//
+//  EstadosView.h
+//  t2_mob_uag
+//
+//  Created by Citlali Garcia on 10/10/14.
+//  Copyright (c) 2014 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EstadosView : UIViewController
+
+@end
