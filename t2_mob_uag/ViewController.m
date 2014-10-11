63431c17b4e9248fb5ce41b58b81bd93f9462ec6
citlalig <citlalig@Citlalis-MacBook-Air.local>
2014-10-10 20:38:22 -0500

    Initial Commit

---
+++ t2_mob_uag/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  t2_mob_uag
+//
+//  Created by Citlali Garcia on 10/10/14.
+//  Copyright (c) 2014 Personal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
